8eb50cb972a06d7b7b991218881b3a1d38e9de86
hung <nguyenthanhhung24102000@gmail.com>
2019-10-01 15:30:33 +0700

    123

---
+++ exercise05/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using exercise05a;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestContains5()
+        {
+            int[] a = new int[] {1, 2, 5, 7, 3};
+            bool result = 
+        }
+    }
+}
---
+++ exercise05/exercise05a/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise05a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            //IsContains5(arr);
+
+        }
+
+        static void Input(int [] arr)
+        {
+           for (int i = 0;i<arr.Length;i++)
+           {
+               Console.Write(" enter value arr{0}=", i);
+               arr[i] = int.Parse(Console.ReadLine());
+
+              
+               }
+           }
+        static bool Iscontains5(int[] arr)
+        {
+            for(int i= 0 ; i)
+        }
+           
+    }
+}
